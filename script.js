503b62cdc407d1eb716633080246457cba2a5789
Hashmat-Noorani <hashmatwani@icloud.com>
2023-01-09 21:51:20 +0530

    testing: full screen scanner

--- script.js
+++ script.js
@@ -446,3 +446,21 @@ function quitScan(e) {
 }
 
 console.log(document.querySelector("body").clientWidth);
+
+function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+  console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  { fps: 10, qrbox: { width: 250, height: 250 } },
+  /* verbose= */ false
+);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
