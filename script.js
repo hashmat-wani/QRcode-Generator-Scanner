7e0c3fa6c1171042d9cc45c382b6daa9072e7161
Hashmat-Noorani <hashmatwani@icloud.com>
2023-01-09 22:51:13 +0530

    testing: full screen mobile scanner

--- script.js
+++ script.js
@@ -357,28 +357,27 @@ document.getElementById("open").addEventListener("click", () => {
 
 const html5QrCode = new Html5Qrcode("reader");
 function scan() {
-  // let cameraId;
   // This method will trigger user permissions
-  Html5Qrcode.getCameras()
-    .then((devices) => {
-      /**
-       * devices would be an array of objects of type:
-       * { id: "id", label: "label" }
-       */
-      if (devices && devices.length) {
-        console.log(devices);
-        let cameraId = devices[1].id;
-        // .. use this to start scanning.
-        html5QrCode.start(
-          { deviceId: { exact: cameraId } },
-          config,
-          qrCodeSuccessCallback
-        );
-      }
-    })
-    .catch((err) => {
-      // handle err
-    });
+  // Html5Qrcode.getCameras()
+  //   .then((devices) => {
+  //     /**
+  //      * devices would be an array of objects of type:
+  //      * { id: "id", label: "label" }
+  //      */
+  //     if (devices && devices.length) {
+  //       console.log(devices);
+  //       let cameraId = devices[1].id;
+  //       // .. use this to start scanning.
+  //       html5QrCode.start(
+  //         { deviceId: { exact: cameraId } },
+  //         config,
+  //         qrCodeSuccessCallback
+  //       );
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     // handle err
+  //   });
 
   const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     /* handle success */
@@ -432,15 +431,15 @@ function scan() {
   const config = {
     fps: 10,
     qrbox: { width: 250, height: 250 },
-    aspectRatio: as,
+    aspectRatio: 1,
   };
 
   // prefering back camera
-  // html5QrCode.start(
-  //   { facingMode: "environment" },
-  //   config,
-  //   qrCodeSuccessCallback
-  // );
+  html5QrCode.start(
+    { facingMode: "environment" },
+    config,
+    qrCodeSuccessCallback
+  );
 }
 
 const cameraAccess = document.querySelector(".fa-camera");
